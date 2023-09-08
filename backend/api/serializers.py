3270ab06a9cdac5b055cf03351e64676ae4471b6
Karkyshka <karkyshakarka@yandex.ru>
2023-09-08 09:58:48 +0300

    Добавление рецепта.фикс ошибки значения

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
+from django.shortcuts import get_object_or_404
 
 from recipes.models import Ingredient, Recipe, Tag, IngredientRecipe
 from user.serializers import CustomUserSerializers
@@ -32,38 +33,21 @@ class IngredientSerializer(ModelSerializer):
 
 class RecipeListSerializer(ModelSerializer):
     """Получение списка рецетов"""
+    is_favorited = serializers.SerializerMethodField()
+    is_in_shopping_cart = serializers.SerializerMethodField()
+
+
     ingredients = IngredientSerializer(many=True, read_only=True)
     author = CustomUserSerializers(read_only=True)
     tags = TagSerializer(many=True, read_only=True)
-
-    is_favorited = serializers.SerializerMethodField()
-    is_in_shopping_cart = serializers.SerializerMethodField()
     image = Base64ImageField()
     class Meta:
         model = Recipe
         fields = (
             'tags', 'author', 'ingredients', 'name', 'cooking_time',
-            'image', 'text', 'amount', 'id', 'is_favorited', 'is_in_shopping_cart'
+            'image', 'text', 'amount', 'id', 'is_favorited',
+            'is_in_shopping_cart'
         )
-    
-    def create(self, validated_data):
-        """Изменение записей в связных таблицах"""
-        # Уберем список достижений из словаря validated_data и сохраним его
-        ingredients_recipe = validated_data.pop('ingredients_recipe')
-
-        # Создадим нового котика пока без достижений, данных нам достаточно
-        recipe = Recipe.objects.create(**validated_data)
-
-        # Для каждого достижения из списка достижений
-        for ingredient in ingredients:
-            # Создадим новую запись или получим существующий экземпляр из БД
-            current_ingredients, status = Ingredient.objects.get_or_create(
-                **ingredient)
-            # Поместим ссылку на каждое достижение во вспомогательную таблицу
-            # Не забыв указать к какому котику оно относится
-            IngredientRecipe.objects.create(
-                ingredients=current_ingredients, recipe=recipe)
-        return recipe 
     
     def get_is_in_shopping_cart(self, ingredient):
         """Список покупок"""
@@ -74,18 +58,37 @@ class RecipeListSerializer(ModelSerializer):
 
 class RecipeActionializer(serializers.ModelSerializer):
     """Работа с рецептами"""
-    author = UserActionSerializer(read_only=True)
-    ingredients = IngredientSerializer(many=True, read_only=True) 
+    ingredients = IngredientSerializer(many=True) 
     tags = TagSerializer(many=True, read_only=True)
     image = Base64ImageField()
 
     class Meta:
         model = Recipe
         fields = (
-            'id', 'tags', 'author', 'ingredients',
-            'name', 'image', 'text', 'cooking_time'
+            'tags', 'ingredients', 'name', 'image', 'text', 'cooking_time'
             )
 
+    def create(self, validated_data):
+        """Изменение записей в связных таблицах"""
+        # request = self.context.get('request')
+        # tag = Tag.objects.create()
+        # ingredients = Tag.objects.create()
+        # recipe.tags.set(tag, ingredients)
+        # recipe = Recipe.objects.create(author=request.user, **validated_data)
+        # return recipe
+        ingredients = validated_data.pop('ingredients')
+        tags = validated_data.pop('tags')
+        request = self.context.get('request')
+        recipe = Recipe.objects.create(author=request.user, **validated_data)
+        recipe.tags.set(tags)
+        ingredient_set = []
+        for ingredient in ingredients:
+            current_ingredient = get_object_or_404(
+                Ingredient,id=ingredient.get('id')
+            )
+            ingredient_set.append(IngredientRecipe(recipe=recipe, ingredient=current_ingredient, tags=tags))
+        return recipe 
+        
     
 
 
