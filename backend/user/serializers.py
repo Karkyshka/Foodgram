03ce930b818722518d42002789f4d113949cd343
Karkyshka <karkyshakarka@yandex.ru>
2023-09-13 09:29:45 +0300

    шаблон валидации на подписки

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -1,15 +1,14 @@
 # from django.contrib.auth import get_user_model
 # from django.db.models import Count
+from django.forms import ValidationError
 from djoser.serializers import UserSerializer
 from rest_framework.serializers import SerializerMethodField
-
+from django.shortcuts import get_object_or_404
 from .models import CustomUser, Subscriber
-
+from rest_framework import status
 
 class CustomUserSerializers(UserSerializer):
-    is_subscribed = SerializerMethodField(read_only=True)
-    # recipe_count = SerializerMethodField()
-    # recipe = SerializerMethodField()
+    is_subscribed = SerializerMethodField()
 
     class Meta:
         model = CustomUser
@@ -18,35 +17,49 @@ class CustomUserSerializers(UserSerializer):
             'first_name', 'last_name', 'is_subscribed'
         )
 
-    def get_is_subscribed(self, obj):
-        request = self.context.get('request')
-        return Subscriber.objects.filter(
-            follower=request.user, following=obj
-        ).exists()
-
-
-class SubscriberSerializers(UserSerializer):
-    """Получение списка подписок."""
-    # is_subscribed = SerializerMethodField(read_only=True)
-    recipes_count = SerializerMethodField()
-    recipes = SerializerMethodField()
-
-    class Meta:
-        model = CustomUser
-        fields = (
-            'email', 'id', 'username', 'first_name',
-            'last_name', 'is_subscribed', 'recipes_count', 'recipes'
-        )
-        read_only_fields = ('email', 'username', 'first_name', 'last_name')
-
     # def get_is_subscribed(self, obj):
     #     request = self.context.get('request')
     #     return Subscriber.objects.filter(
     #         follower=request.user, following=obj
     #     ).exists()
 
+
+class SubscriberSerializers(UserSerializer):
+    """Получение списка подписок."""
+    recipes_count = SerializerMethodField()
+    recipes = SerializerMethodField()
+    is_subscribed = SerializerMethodField()
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'email', 'id', 'username', 'first_name',
+            'last_name', 'is_subscribed', 'recipes', 'recipes_count'
+        )
+        # read_only_fields = ('email', 'username', 'first_name', 'last_name')
+
+      
+        def validate(self, data):
+            following_id = self.context.get(
+                'request').parser_context.get('kwargs').get('id')
+            request = self.context.get('request')
+            follower = request.user
+            following = get_object_or_404(CustomUser, id=following_id)
+            if request.method == 'POST':
+                if Subscriber.objects.filter(
+                    follower=follower, following=following
+                ):
+                    raise ValidationError('Вы уже подписаны на этого автора')
+            return data
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        return Subscriber.objects.filter(
+            follower=request.user, following=obj
+        ).exists()
+
     def get_recipes_count(self, obj):
         return obj.recipes.count()
 
     def get_recipes(self, obj):
-        request
+        pass
