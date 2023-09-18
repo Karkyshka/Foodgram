ef66d27a724bd464f7bbc217a33376898a9d034e
Karkyshka <karkyshakarka@yandex.ru>
2023-09-18 17:28:14 +0300

    STATIC_ROOT

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,8 +123,8 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = BASE_DIR / STATIC_URL
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = BASE_DIR / STATIC_URL
 # STATICFILES_DIRS = (
 #     os.path.join(BASE_DIR, 'static'),
 # )
