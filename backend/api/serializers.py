6233bf8b672c74a454d7839343c194cee2420559
Karkyshka <karkyshakarka@yandex.ru>
2023-09-24 13:00:03 +0300

    isort

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,10 +1,9 @@
 from djoser.serializers import UserSerializer
 from drf_extra_fields.fields import Base64ImageField
-from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
-
 from recipes.models import (Favorite, Ingredient, IngredientRecipe, Recipe,
                             ShoppingCart, Tag)
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from user.models import CustomUser
 from user.serializers import CustomUserSerializers
 
