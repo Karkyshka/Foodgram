e051a5e58ec1f3b28243684d7bf150e46bce12e5
Karkyshka <karkyshakarka@yandex.ru>
2023-09-26 15:02:45 +0300

    рецепты анониму

--- backend/api/permission.py
+++ backend/api/permission.py
@@ -10,6 +10,3 @@ class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in SAFE_METHODS
                 or obj.author == request.user or request.user.is_admin())
-
-    # def has_object_permission(self, request, view, obj):
-    #     return (request.method in SAFE_METHODS or obj.author == request.user)
