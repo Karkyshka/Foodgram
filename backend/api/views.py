512852f01d04b46499440ffdafca1184624c09f2
Karkyshka <karkyshakarka@yandex.ru>
2023-09-26 15:11:23 +0300

    рецепты анониму

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.permissions import (AllowAny, IsAuthenticated,
+from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
