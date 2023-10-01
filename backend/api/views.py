28356be2170dcc6a5be766a0ea2204dfe70449f4
Karkyshka <karkyshakarka@yandex.ru>
2023-10-01 19:32:02 +0300

    На ревью

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,6 @@ from api.serializers import (FavoriteSerializer, IngredienSerializer,
                              RecipeActionializer, RecipeListSerializer,
                              ShoppingCartSerializer, TagSerializer)
 from django.db.models import Sum
-from django.http import HttpResponse
 from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
